f8fb91abe708cc5e21a2baec363c16b4aa484a3f
NicoNeko <nicuch100@gmail.com>
2026-03-12 12:21:46 +0200

    Reload on global/main thread

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandReload.kt
@@ -1,5 +1,6 @@
 package com.willfp.ecopets.commands
 
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.eco.util.StringUtils
 import com.willfp.eco.util.toNiceString
@@ -13,9 +14,15 @@ object CommandReload : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        sender.sendMessage(
-            plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
-                .replace("%time%", plugin.reloadWithTime().toNiceString())
-        )
+        val runnable = Runnable {
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+            )
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable)
+        else
+            runnable.run()
     }
 }
